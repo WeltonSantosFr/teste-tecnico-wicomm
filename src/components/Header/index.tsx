32a5fb8875e5d944a144067fb25855eaab455fed
=Welton Santos <weltonsantosfr@gmail.com>
2023-04-14 16:13:20 -0300

    feat:header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -26,36 +26,49 @@ const Header = () => {
             alt="Black Skull Logo"
             className="w-44 -ml-2 hover:cursor-pointer"
           />
-          <button className="font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 hover:h-[72px]">
-            CATEGORIAS
-          </button>
-          <button className="font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 hover:h-[72px]">
-            VESTUÁRIO
-          </button>
-          <button className="font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 hover:h-[72px]">
-            OBJETIVOS
-          </button>
-          <button className="font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 hover:h-[72px]">
-            PROMOÇÕES
-          </button>
-          <button className="font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 hover:h-[72px]">
-            ATLETAS
-          </button>
-          <button className="font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 hover:h-[72px]">
-            ASSINATURA
-          </button>
-          <div className="w-[216px] h-[40px] flex bg-dark1 items-center justify-evenly border-[1px] border-dark3 rounded">
+          <div className="flex">
+            <button className="w-[103px] h-[72px] font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 relative items-center transition ease-in-out duration-150 hover:text-white1">
+              <span>CATEGORIAS</span>
+              <div className="h-1 w-0 bg-orange1 absolute bottom-0 left-0 transition-all duration-500"></div>
+            </button>
+
+            <button className="w-[103px] h-[72px] font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 relative items-center transition ease-in-out duration-150 hover:text-white1">
+              <span>VESTUÁRIO</span>
+              <div className="h-1 w-0 bg-orange1 absolute bottom-0 left-0 transition-all duration-500"></div>
+            </button>
+
+            <button className="w-[103px] h-[72px] font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 relative items-center transition ease-in-out duration-150 hover:text-white1">
+              <span>OBJETIVOS</span>
+              <div className="h-1 w-0 bg-orange1 absolute bottom-0 left-0 transition-all duration-500"></div>
+            </button>
+
+            <button className="w-[103px] h-[72px] font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 relative items-center transition ease-in-out duration-150 hover:text-white1">
+              <span>PROMOÇÕES</span>
+              <div className="h-1 w-0 bg-orange1 absolute bottom-0 left-0 transition-all duration-500"></div>
+            </button>
+
+            <button className="w-[103px] h-[72px] font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 relative items-center transition ease-in-out duration-150 hover:text-white1">
+              <span>ATLETAS</span>
+              <div className="h-1 w-0 bg-orange1 absolute bottom-0 left-0 transition-all duration-500"></div>
+            </button>
+
+            <button className="w-[103px] h-[72px] font-barlow font-bold text-[0.875rem] text-grey1 text-center hover:bg-dark2 relative items-center transition ease-in-out duration-150 hover:text-white1">
+              <span>ASSINATURA</span>
+              <div className="h-1 w-0 bg-orange1 absolute bottom-0 left-0 transition-all duration-500"></div>
+            </button>
+          </div>
+          <div className="w-[216px] h-[40px] flex bg-dark1 items-center justify-evenly border-[1px] border-dark3 rounded hover:bg-dark2">
             <input
               type="text"
               placeholder="Buscar"
               name="busca"
-              className="font-barlow font-medium text-[12px] w-[80%] h-[100%] outline-none bg-dark1 text-grey1 focus:placeholder:opacity-0"
+              className="font-barlow font-medium text-[12px] w-[80%] h-[100%] outline-none bg-dark1 text-grey1 focus:placeholder:opacity-0 hover:bg-dark2"
             />
             <BiSearch className="text-grey1 hover:cursor-pointer" />
           </div>
-          <BiUserCircle className="text-grey1 w-5 h-5" />
-          <BiStar className="text-grey1 w-5 h-5" />
-          <BiCart className="text-grey1 w-5 h-5" />
+          <BiUserCircle className="text-grey1 w-5 h-5 hover:cursor-pointer hover:text-white1" />
+          <BiStar className="text-grey1 w-5 h-5 hover:cursor-pointer hover:text-white1" />
+          <BiCart className="text-grey1 w-5 h-5 hover:cursor-pointer hover:text-white1" />
         </div>
       </div>
     </header>
